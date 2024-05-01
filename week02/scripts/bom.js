53971309fcc3de505d99ebcb19b338a69a6e0c55
alexanderpacherrez <pacherrezberru@gmail.com>
2024-05-01 08:12:55 -0500

    changes

---
+++ week02/scripts/bom.js
@@ -0,0 +1,12 @@
+const input = document.querySelector("favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("ul");
+
+const newLi = document.createElement("li");
+const deleteButton = document.createElement("button");
+
+newLi.textContent = input.value;
+deleteButton.textContent = "❌";
+
+newLi.append(deleteButton);
+list.append(newLi);
