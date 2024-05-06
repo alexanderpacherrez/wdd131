194e5b1489b1201819f83d4d848425a804d652ba
alexanderpacherrez <pacherrezberru@gmail.com>
2024-05-06 00:39:49 -0500

    changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,15 @@ const today = new Date();
 
 currentYear.innerHTML = today.getFullYear();
 lastModified.innerHTML = document.lastModified;
+
+
+// const hamButton = document.querySelector("#ham-menu");
+// const navigation = document.querySelector("navigation");
+
+// hamButton.addEventListener("click",() => {
+//     navigation.classList.toggle("open");
+//     hamButton.classList.toggle("open");
+// });
+
+
+
