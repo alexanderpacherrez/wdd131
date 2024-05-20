f9911cf34b1c8fcf67f34599a574ca4818489b70
alexanderpacherrez <pacherrezberru@gmail.com>
2024-05-20 09:01:14 -0500

    changes

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -89,5 +89,38 @@ const temples = [
     },
   ];
 
+createTempleCard(temples);
+
+function createTempleCard(FilteredTemples) {
+  document.querySelector(".res-grid").innerHTML = "";
+  filteredTemples.forEach(temple => {
+    let card = document.createElement("section");
+    let name = document.createElement("h3");
+    let location = document.createElement("p");
+    let dedication = document.createElement("p");
+    let area = document.createElement("p");
+    let img = document.createElement("img");
+    
+    name.textContent = temple.templeName;
+    location.innerHTML = `<span class="label">Location:</span> ${temple.location}`;
+    dedication.innerHTML = `<span class="label">Dedicated:</span> ${temple.dedicated}`;
+    area.innerHTML = `<span class="label">Size:</span> ${temple.area} sq ft`;
+    img.setAttribute("src", temple.imageUrl);
+    img.setAttribute("alt", `${temple.templeName} Temple`);
+    img.setAttribute("loading", "lazy");
+
+    card.appendChild(name);
+    card.appendChild(location);
+    card.appendChild(dedication);
+    card.appendChild(area);
+    card.appendChild(img);
+    
+    document.querySelector(".res-grid").appendChild(card);
+
+
+  }
+  )
+}
+
 
 
