27c45d750dbf2077892f71058419bcda9b7e29c7
alexanderpacherrez <pacherrezberru@gmail.com>
2024-05-29 21:27:11 -0500

    changes

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -89,38 +89,6 @@ const temples = [
     },
 ];
 
-/*menu*/
-let itemMenu = document.querySelectorAll('.random-menu');
-
-ItemMenu.forEach( menu => {
-  menu.addEventListener( 'click', function(){
-    let menuRel = menu.getAttribute('rel');
-    let filteredData;
-    if( menuRel == 'old' || menuRel == 'new' )
-    {
-      // console.log(temple.dedicated.getFullYear());
-      if( menuRel == 'old') {
-        filteredData = temples.filter(temple => !checkOldNew(temple.dedicated));
-      }else{
-        filteredData = temples.filter(temple => checkOldNew(temple.dedicated));
-      }
-    }
-    else if(menuRel == 'large' || menuRel == 'small'){
-      if( menuRel == 'large') {
-        filteredData = temples.filter( temple => temple.area>900000);
-      }else{
-        filteredData = temples.filter( temple =>temple.area<100000);
-      }
-
-    }
-    else {
-      //home
-      filteredData = temples.filter( temple => temple !== undefined );
-    }
-    
-    createTemplateCard(filteredData);
-  });
-});
 
 function checkOldNew( dateOldNew ) {
   const date1 = new Date(dateOldNew)
